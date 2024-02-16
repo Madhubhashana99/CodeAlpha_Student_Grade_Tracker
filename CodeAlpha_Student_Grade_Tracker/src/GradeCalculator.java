d97b7d967b315db3198d4912528724440e4eea7e
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-02-17 00:18:51 +0530

    update GradeCalculater

--- CodeAlpha_Student_Grade_Tracker/src/GradeCalculator.java
+++ CodeAlpha_Student_Grade_Tracker/src/GradeCalculator.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of students: ");
+        int numStudents = scanner.nextInt();
+
+        //Create array to store grade
+        int[] grades = new int[numStudents];
+
+        //Input grades from the teacher
+        for(int i=0;i<numStudents;i++){
+            System.out.println("Enter the grade for student " + (i+1)+"; ");
+            grades[i] = scanner.nextInt();
+        }
+
+        //Compute average, highest and lowest scores
+        int sum = 0;
+        int highest = grades[0];
+        int lowest = grades[0];
+
+        for(int grade:grades){
+            
+        }
+
+    }
 }
